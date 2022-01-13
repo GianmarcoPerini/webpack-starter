831cfcca8588de3b1789dab863a668d6a9547dec
GianmarcoPerini <g.marco.perini@gmail.com>
2022-01-13 14:38:09 +0100

    init webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './scss/style.scss';
+
+const h1 = document.querySelector('h1');
+
+h1.innerText = 'Hello i am your personal & definitive webpack file';
+console.log(s);
+
+if (module.hot) module.hot.accept();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    assetModuleFilename: 'img/[hash][ext][query]',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
